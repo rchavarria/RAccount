c891b59d71f31941937897b05c74e019e01c2514
Ruben Chavarria <txingo@gmail.com>
2012-06-15 13:24:46 +0200

    added basic validations on dates before querying for expenses

--- src/main/java/es/rchavarria/raccount/frontend/expensesReport/gui/ExpensesReportAcceptAction.java
+++ src/main/java/es/rchavarria/raccount/frontend/expensesReport/gui/ExpensesReportAcceptAction.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.util.Date;
 
 import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
 
 import es.rchavarria.raccount.model.Account;
 
@@ -22,8 +23,42 @@ public class ExpensesReportAcceptAction extends AbstractAction {
         Date dateFrom = view.getDateFrom();
         Date dateTo = view.getDateTo();
         
+        if(dateFrom == null){
+            showDateFromError();
+            return;
+        }
+        
+        if(dateTo == null){
+            showDateToError();
+            return;
+        }
+        
+        if(dateTo.before(dateFrom)){
+            showPeriodError();
+            return;
+        }
+        
         ExpensesReportWorker worker = new ExpensesReportWorker(account, dateFrom, dateTo, view);
         worker.execute();
+    }
+
+    private void showError(String msg) {
+        JOptionPane.showMessageDialog(view, 
+                                      msg, 
+                                      "Error requesting expenses", 
+                                      JOptionPane.ERROR_MESSAGE);
+    }
+
+    private void showDateFromError() {
+        showError("Date 'from' can not be empty");
+    }
+
+    private void showDateToError() {
+        showError("Date 'to' can not be empty");
+    }
+    
+    private void showPeriodError() {
+        showError("Date 'to' can not be before date 'from'");
     }
 
 }
