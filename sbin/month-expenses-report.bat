7f64a1abeb1d15f5c3b1bd6d89d83368320f5e42
Ruben Chavarria <txingo@gmail.com>
2012-07-02 12:22:47 +0200

    fixed windows shell scripts to launch some functionalities of the application

--- sbin/month-expenses-report.bat
+++ sbin/month-expenses-report.bat
@@ -2,7 +2,8 @@ echo off
 
 call create-classpath.bat
 
-set MAIN_CLASS=es.rchavarria.raccount.frontend.script.TotalExpensesReporter
+set MAIN_CLASS=es.rchavarria.raccount.frontend.script.TotalExpensesReporterGUI
 @rem echo %MAIN_CLASS%
 
 java -cp %PRIVATE_CLASSPATH% %MAIN_CLASS%
+pause
