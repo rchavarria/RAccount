ca2602e8e1054508305f6f6c5d5a993adb49816a
Ruben Chavarria <txingo@gmail.com>
2012-07-02 12:11:51 +0200

    moved a test class to tests directory

--- src/test/java/es/rchavarria/raccount/bussines/util/DateUtilsTest.java
+++ src/test/java/es/rchavarria/raccount/bussines/util/DateUtilsTest.java
@@ -1,6 +1,6 @@
 package es.rchavarria.raccount.bussines.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Calendar;
 import java.util.Date;
